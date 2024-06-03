bc1510770a646687708688fe19f264729f7e9c77
ochaklosh <oksana.chaklosh@testpro.io>
2024-06-03 03:12:05 -0400

    Updated BaseTest and LoginTests
    Cleaned up LoginTests class by moving repeatable code to BaseTest class
    Used refactoring to speed up the process

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,61 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+    WebDriver driver = null;
+    ChromeOptions options = new ChromeOptions();
+    String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
-    //This is a change
+
+    @BeforeMethod
+    public void launchBrowser(){
+        //Pre-Condition
+        //Added ChromeOptions argument below to fix websocket error
+       options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+        }
+
+    protected void navigateToPage() {
+        driver.get(url);
+    }
+
+    protected void enterEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    protected void submit() {
+        WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginBtn.click();
+    }
+
+    protected void enterPassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("te$t$tudent");
+    }
 }
+
+    //This is a change
