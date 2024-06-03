0cd82e213408ee5de5982db7108a5fd78d0e2b49
James <james.mar@testpro.io>
2024-06-02 20:39:48 -0400

    Updated Homework-16
    Updated Homework-16 streamline the code

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,28 +1,32 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void registrationNavigation(){
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        navigateToPage();
+        Assert.assertEquals(driver.getCurrentUrl(), urlLogin);
+        driver.findElement(By.linkText("Registration / Forgot password")).click();
+    }
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
-        driver.get(url);
+    @Test(enabled=false)
+    public void loginValidEmailPassword() throws InterruptedException {
 
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        navigateToPage();
+        Assert.assertEquals(driver.getCurrentUrl(), urlLogin);
+        enterEmail("demo@testpro.io");
+        enterPassword("te$t$tudent");
+        submit();
+        Assert.assertEquals(driver.getCurrentUrl(), urlHome);
 
     //    driver.quit();
     }
