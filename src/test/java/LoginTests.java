28b775785aa99a859fbe5d15ab9ae3325d482dfa
Aidnim <india.messam@testpro.io>
2024-06-04 08:42:44 -0400

    Updated URL
    URL changed to test pro

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -16,8 +16,8 @@ public class LoginTests extends BaseTest {
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
+//      Changing url test hw15
+        String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
