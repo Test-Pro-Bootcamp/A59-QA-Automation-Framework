ddaca3f93064db50ac44642e254b7bf982c64cb4
aleksei1990 <aleksei.koksharov@testpro.io>
2024-06-03 22:46:45 -0400

    Homework 16
    Registration

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-public class homework16 {
+public class Homework16 extends BaseTest {
     @Test
     public void  registrationNavigation()  {
 
