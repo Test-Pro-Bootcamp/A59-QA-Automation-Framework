c1d5bee17b71fcc39b7d495fd6155ee94b253bf2
James <james.mar@testpro.io>
2024-06-04 02:17:04 -0400

    Homework-17
    Homework-17

--- src/test/java/PlayListTests.java
+++ src/test/java/PlayListTests.java
@@ -1,9 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.UUID;
 
 public class PlayListTests extends BaseTest{
 
