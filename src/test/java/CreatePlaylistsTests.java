e661dd1733a6e4e20c9264fa8226ddee7efdeb01
KristinaSarkisyan <kristina.sarkisyan@testpro.io>
2024-06-07 01:49:16 -0500

    Homework-19 Completed

--- src/test/java/CreatePlaylistsTests.java
+++ src/test/java/CreatePlaylistsTests.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class createPlaylistsTests extends BaseTest {
+public class CreatePlaylistsTests extends BaseTest {
     @Test
     public void createPlaylist() throws InterruptedException {
         String expectedCreatedPlaylistMessage = "Created playlist \"Kristina.\"";
