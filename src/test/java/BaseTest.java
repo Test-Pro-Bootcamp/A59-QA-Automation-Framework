4e41c730468f0ff86518531017bcefe6afc90368
James <james.mar@testpro.io>
2024-06-08 11:16:41 -0400

    Homework-17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -16,28 +17,39 @@ public class BaseTest {
     ChromeOptions options = new ChromeOptions();
     String url = "https://qa.koel.app/";
 
+//    WebDriverWait wait;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeMethod
-    public void launchBrowser(){
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL) {
         // Pre-condition
         // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        url = BaseURL;
+        navigateToPage();
+
+   //     wait = new WebDriverWait;
     //    driver.manage().window().maximize();
     }
 
     @AfterMethod
     public void closeBrowser(){
-     //   driver.quit();
+        driver.quit();
     }
 
     protected void submit() throws InterruptedException {
+      //  WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+
         submit.click();
         Thread.sleep(1000);
     }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,24 +1,35 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 public class LoginTests extends BaseTest {
 
-    @Test
+    @DataProvider(name="IncorrectLoginData")
+    public Object[][]getDataFromDataProviders() {
+        return new Object[][] {
+                {"invalid@testpro.io", "invalidPassword"},
+                {"demo@testpro.io", ""},
+                {"",""}
+        };
+    }
+
+    @Test (enabled = false)
     public void loginValidEmailPassword() throws InterruptedException {
 
         navigateToPage();
         enterEmail("demo@testpro.io");
         enterPassword("te$t$tudent");
         submit();
-
+    //    WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
         WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
         // Expected Result
         Assert.assertTrue(avatarIcon.isDisplayed());
     }
 
-    @Test
+    @Test (enabled = false)
     public void loginInvalidEmailValidPassword() throws InterruptedException {
 
         navigateToPage();
@@ -28,20 +39,30 @@ public class LoginTests extends BaseTest {
         enterPassword("te$t$tudent");
         submit();
 
-        Thread.sleep(2000); // Sleep or pause for 2 seconds (adjust as needed)
+        Thread.sleep(1000); // Sleep or pause for 2 seconds (adjust as needed)
         // Expected Result
         Assert.assertEquals(driver.getCurrentUrl(), url); // https://qa.koel.app/
     }
 
-    @Test
+    @Test (enabled = false)
     public void loginValidEmailEmptyPassword() throws InterruptedException {
-
         navigateToPage();
         enterEmail("invalid@testpro.io");
         submit();
 
-        Thread.sleep(2000); // Sleep or pause for 2 seconds (adjust as needed)
+        Thread.sleep(1000); // Sleep or pause for 2 seconds (adjust as needed)
         // Expected Result
         Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
+    }
+
+    @Test (dataProvider = "IncorrectLoginData")
+    public void loginEmptyEmailPassword(String email, String password) throws InterruptedException {
+        navigateToPage();
+        enterEmail(email);
+        enterPassword(password);
+        submit();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
+
     }
 }
