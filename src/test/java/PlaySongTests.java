40cc7528812c04c6faba8a9a13b0a1aba1d05834
James <james.mar@testpro.io>
2024-06-08 11:36:49 -0400

    Homework-17

--- src/test/java/PlaySongTests.java
+++ src/test/java/PlaySongTests.java
@@ -10,50 +10,36 @@ public class PlaySongTests extends BaseTest {
 
         String expectedMessage = null;
 
-        //Standard procedures
+        // Given
         navigateToPage();
         enterEmail("demo@testpro.io");
         enterPassword("te$t$tudent");
         submit();
         Thread.sleep(3000);
+
+        // When
         playNextSongBtn();
         playSongBtn();
         findSoundBars();
-     //   Assert.assertEquals(findSoundBars(), expectedMessage);
+
+        // Then
+        Assert.assertTrue(findSoundBars());
     }
 
     public void playNextSongBtn() throws InterruptedException {
         WebElement playNextSong = driver.findElement(By.xpath("//i[@role=\"button\"][@title=\"Play next song\"][@data-testid=\"play-next-btn\"]"));
-
-    //    System.out.println("Begin");
-    //    System.out.println(playNextSong.toString());
-    //    System.out.println("End");
-
         playNextSong.click();
-
         Thread.sleep(3000);
     }
+
     public void playSongBtn() throws InterruptedException {
         WebElement playSong = driver.findElement(By.xpath("//span[@title=\"Play or resume\"][@data-testid=\"play-btn\"]//i[@class=\"fa fa-play\"]"));
-
-    //    System.out.println("Begin");
-    //    System.out.println(playSong.toString());
-    //    System.out.println("End");
-
         playSong.click();
-
         Thread.sleep(3000);
     }
 
-    public void findSoundBars() throws InterruptedException {
-        WebElement verifySoundBar = driver.findElement(By.xpath("//div[@data-test=\"soundbars\"]"));
-        System.out.println("Begin");
-        System.out.println(verifySoundBar.toString());
-        if (verifySoundBar.toString() != null) {
-            System.out.println("Successfully Playing Song");
-        } else {
-            System.out.println("Failed to Play Song");
-        }
-        System.out.println("End");
+    public boolean findSoundBars() throws InterruptedException {
+        WebElement verifySoundBar = driver.findElement(By.xpath("//div[@data-test=\"soundbars\"][@class=\"bars\"][@data-testid=\"sound-bar-play\"]"));
+        return verifySoundBar.isDisplayed();
     }
 }
