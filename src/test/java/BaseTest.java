985090ca17453f16b51e7dcbe13aca418e583188
ochaklosh <oksana.chaklosh@testpro.io>
2024-06-10 02:00:48 -0400

    Updated BaseTest
    Added multiple arguments to a configuration, related to launching and configuring Google Chrome.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -29,7 +29,27 @@ public class BaseTest {
     public void launchBrowser(){
         //Pre-Condition
         //Added ChromeOptions argument below to fix websocket error
+        /*--remote-allow-origins=* argument suggests allowing remote origins. It means that
+        the application will accept requests from any remote origin, which can be helpful for testing
+        scenarios where requests are made from various sources.
+        */
        options.addArguments("--remote-allow-origins=*");
+       /*--disable-notifications argument suggests disabling notifications. This could be useful during
+       automated testing to prevent interruptions from notifications that may interfere with the testing process.
+        */
+       options.addArguments("--disable-notifications");
+       /*--incognito argument indicates launching the application in incognito mode. Incognito mode typically
+       means that the browser doesn't store browsing history or cookies, which can be beneficial for testing
+       scenarios where a clean browsing session is required.
+        */
+       options.addArguments("--incognito");
+       options.addArguments("--start-maximized");
+       /*This setting seems to be specific to ChromeDriver, a tool for automated testing with Google Chrome.
+       It suggests excluding the switch enable-automation, which is a Chrome feature indicating that Chrome is
+       being controlled by automated test software. By excluding this switch, it can help avoid detection by
+       websites or applications that may behave differently when they detect automated testing.
+        */
+       options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
