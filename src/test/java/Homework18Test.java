2ac1a8fa5ad00e9ae2afcf6053d20b928b7582ff
KristinaSarkisyan <kristina.sarkisyan@testpro.io>
2024-06-10 01:45:41 -0500

    Update Homework18Test.java

--- src/test/java/Homework18Test.java
+++ src/test/java/Homework18Test.java
@@ -9,7 +9,6 @@ import java.time.Duration;
 public class Homework18Test extends BaseTest{
     @Test
     public void playSong()  {
-
  //navigateToPage();
  enterEmail("kristina.sarkisyan@testpro.io");
  enterPassword("o8URUDnW");
