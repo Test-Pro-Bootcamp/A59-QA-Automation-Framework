475cc5c84c1ef55ca6a0e7e8f5e069be0e125942
Aidnim <india.messam@testpro.io>
2024-06-11 21:06:59 -0400

    HW 21 Rename playlist
    Used Action double click to edit name of playlist. Playlist was changed for Barrett to love. The only error seems to be the assert but it looks correct to me.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -13,31 +17,40 @@ import java.time.Duration;
 
 public class BaseTest {
     public WebDriver driver = null;
+    WebDriverWait wait;
+    Wait<WebDriver>fluentWait;
+    Actions actions;
 
-    public String url = "https://qa.koel.app/";
+    //public String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
-
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeMethod
     @Parameters({"baseURL"})
     public void launchBrowser (String baseURL){
+        //pre-condition
+        //added ChromeOptions
         ChromeOptions optionsChromeLocal = new ChromeOptions();
         optionsChromeLocal.addArguments("--disable-notifications","--remote-allow-origins=*","incognito","--start-maximized","-lang=en");
         driver = new ChromeDriver(optionsChromeLocal);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30));
+        actions = new Actions(driver);
         driver.get(baseURL);
-        url = baseURL;
-
 
     }
 
     @AfterMethod
     public void closeBrowser(){
-        driver.quit();
+        if(driver !=null){
+            driver.quit();
+        }
+
     }
     public void login(String email, String password){
         WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
