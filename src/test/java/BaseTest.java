ce2f98ec3e4c37555987008054f52505f1cf0f8f
arafiqui <ayoub.rafiqui@testpro.io>
2024-06-12 14:38:57 -0700

    HomeWork21
    created rename playlist test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,10 @@
+import com.beust.ah.A;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
@@ -25,6 +27,8 @@ String password = "te$t$tudent";
         WebDriverManager.chromedriver().setup();
     }
 
+    public static Actions actions = null;
+
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void launchBrowser(String baseURL) {
@@ -34,8 +38,10 @@ String password = "te$t$tudent";
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
          wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+         actions = new Actions(driver);
         url = baseURL;
         driver.get(url);
+
     }
 
 
