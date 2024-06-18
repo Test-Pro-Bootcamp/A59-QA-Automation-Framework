93042854a6c105c3e173bcd670c7f02da402a018
TestProGio <giovanna.silva@testpro.io>
2024-06-18 08:43:01 -0400

    homework23 updated
    Applied your changes but a bit confused on this change to 'PlaylistActions':   // LOCATORS
        @FindBy(linkText = "Delete2Sample Edited PlaylistSample Edited PlaylistSample Edited PlaylistSample Edited Playlist")
        private WebElement clickPlaylist;

--- src/test/java/pgFactoryPages/BasePage.java
+++ src/test/java/pgFactoryPages/BasePage.java
@@ -1,5 +1,5 @@
-package pgFactoryPages;
 
+/*
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -44,7 +44,50 @@ public class BasePage {
         return findElement(soundBarVisualizer).isDisplayed();
 
     }
+}
 
+ */
+package pgFactoryPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 
+import java.time.Duration;
+
+public class BasePage {
+    // HW Feedback: Use 'protected' access since it gives the subclass a chance to use the helper method or variable, while preventing a non-related class from trying to use it.
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
+
+    By soundBarVisualizer = By.cssSelector("[data-testid = 'sound-bar-play']");
+
+    public BasePage(WebDriver givenDriver) {
+        driver = givenDriver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        actions = new Actions(driver);
+    }
+
+    // Add the waitForPresence method
+    public WebElement waitForPresence(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    // Add the waitForClickable method
+    public WebElement waitForClickable(WebElement element) {
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    // New methods added
+    protected WebElement findElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected boolean isSongPlaying() {
+        return findElement(soundBarVisualizer).isDisplayed();
+    }
 }
--- src/test/java/pgFactoryPages/PlaylistActions.java
+++ src/test/java/pgFactoryPages/PlaylistActions.java
@@ -1,4 +1,4 @@
-package pgFactoryPages;
+/*package pgFactoryPages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -43,6 +43,52 @@ public class PlaylistActions {
 
     public void successPopUpBox() {
         WebElement successPopUpElement = wait.until(ExpectedConditions.visibilityOf(successPopUp));
+        System.out.println("Success Popup Displayed");
+    }
+}
+
+ */
+package pgFactoryPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PlaylistActions extends BasePage {
+
+
+    // Constructor
+    public PlaylistActions(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+
+    // LOCATORS
+    @FindBy(linkText = "Delete2Sample Edited PlaylistSample Edited PlaylistSample Edited PlaylistSample Edited Playlist")
+    private WebElement clickPlaylist;
+
+    @FindBy(css = "button[title='Delete this playlist']")
+    private WebElement deletePlaylistAction;
+
+    @FindBy(css = "div.success.show")
+    private WebElement successPopUp;
+
+    // Page Methods
+    public void selectPlayList() {
+        waitForPresence(clickPlaylist).click();
+        System.out.println("Clicked Playlist");
+    }
+
+    public void deletePlaylist() {
+        waitForClickable(deletePlaylistAction);
+        System.out.println("Deleted Playlist");
+    }
+
+    public void successPopUpBox() {
+        waitForPresence(successPopUp);
         System.out.println("Success Popup Displayed");
     }
 }
