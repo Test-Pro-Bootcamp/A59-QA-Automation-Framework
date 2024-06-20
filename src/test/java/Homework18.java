bd3092a370476788deb8febf826a99560e2dca77
aleksei1990 <aleksei.koksharov@testpro.io>
2024-06-19 19:24:24 -0500

    Homework18

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+
+    @Test
+
+    public void playSong() throws InterruptedException {
+
+        //login
+        navigateToPage();
+        enterEmail("aleksei.koksharov@testpro.io");
+        enterPassword("ak1234!@#$");
+        submit();
+        Thread.sleep(4000);
+        //from Home page click play button
+        clickPlayBtn();
+        Thread.sleep(2000);
+        validatePauseButton();
+    }
+
+
+
+    public void clickPlayBtn() {
+            WebElement playBtn = driver.findElement(By.cssSelector("span.play"));
+            playBtn.click();
+        }
+        public void validatePauseButton() {
+            WebElement pauseBtn = driver.findElement(By.cssSelector("span.pause"));
+            //Assertions - Expected vs Actual
+            Assert.assertTrue(pauseBtn.isDisplayed());
+        }
+
+
+    }
