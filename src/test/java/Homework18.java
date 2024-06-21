2fe3b166f94518167a13ae2783243bbcb80aabc5
aleksei1990 <aleksei.koksharov@testpro.io>
2024-06-20 22:15:57 -0500

    Homework18-UPDATE

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import javax.swing.*;
 
 public class Homework18 extends BaseTest {
 
@@ -14,24 +17,34 @@ public class Homework18 extends BaseTest {
         enterEmail("aleksei.koksharov@testpro.io");
         enterPassword("ak1234!@#$");
         submit();
+        driver.manage().window().maximize();
         Thread.sleep(4000);
+        enterAllSongs();
         //from Home page click play button
         clickPlayBtn();
         Thread.sleep(2000);
-        validatePauseButton();
-    }//
+        //Assertions - Expected vs Actual
+        Assert.assertTrue(isDisplayedSoundBar());
+    }
 
+    private boolean isDisplayedSoundBar() {
+        WebElement soundBar = driver.findElement(By.cssSelector("img[alt='Sound bars']"));
+        return soundBar.isDisplayed();
+    }
+
+    private void enterAllSongs() {
+        WebElement allSongs = driver.findElement(By.cssSelector("a[href='#!/songs']"));
+        allSongs.click();
+    }
 
 
     public void clickPlayBtn() {
             WebElement playBtn = driver.findElement(By.cssSelector("span.play"));
-            playBtn.click();
+            Actions actions = new Actions(driver);
+            actions.click(playBtn).perform();
+
         }
-        public void validatePauseButton() {
-            WebElement pauseBtn = driver.findElement(By.cssSelector("span.pause"));
-            //Assertions - Expected vs Actual
-            Assert.assertTrue(pauseBtn.isDisplayed());
-        }
+
 
 
     }
