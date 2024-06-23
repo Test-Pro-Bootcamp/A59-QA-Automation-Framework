11c037130c0c857866875bbcbccfa31a8c6e4238
btorelli89 <brittany.torelli@testpro.io>
2024-06-23 16:58:38 -0500

    HomeWork23
    HomeWork 23 (Used Test Case HomeWork18)

---
+++ src/test/java/homeWork18.java
@@ -0,0 +1,27 @@
+
+import Pages.AllSongsPage;
+import Pages.HomePage;
+import Pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class homeWork18 extends BaseTest{
+
+
+    @Test
+    public void playSong(){
+
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        AllSongsPage allSongsPage = new AllSongsPage(driver);
+
+        loginPage.provideEmail("demo@testpro.io");
+        loginPage.providePassword("te$t$tudent");
+        loginPage.clickSubmit();;
+        homePage.chooseAllSongsList();
+        allSongsPage.contextClickFirstSong();
+        allSongsPage.choosePlayOption();
+
+        allSongsPage.validateSongPlaying();
+    }
+}
