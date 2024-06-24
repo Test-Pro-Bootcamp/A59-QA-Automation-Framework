e9e888d73c1abe36ca4a62503c6779504e1ea142
KristinaSarkisyan <kristina.sarkisyan@testpro.io>
2024-06-24 02:16:14 -0500

    Update ProfileTest.java

--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,8 +1,13 @@
+import page.HomePage;
 import page.LoginPage;
 
 public class ProfileTest extends BaseTest{
 
     public void changeCurrentTheme() {
         LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+        ProfilePage profilePage = new ProfilePage(getDriver());
+
+        loginPage.provideEmail("kristina.sarkisyan@testpro.io");
     }
 }
