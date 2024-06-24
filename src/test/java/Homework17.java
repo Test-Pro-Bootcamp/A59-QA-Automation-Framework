9571ebf36268437df2d43503a7b46bae1b7b5d46
testprolenp <leon.poyau@testpro.io>
2024-06-24 18:41:21 -0400

    Added code to do assertion validation based on assertEquals()
    Not working yet

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,40 +1,45 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
     @Test
     public void addSongToPlaylist() throws InterruptedException {
+        // Pre-requisite values
         String chosenPlayList = "LeonPlayList";
+        String songToSearch = "dark";
+
         navigateToHomePage();
         login();
 
         // Check initial length of playlist
-        String len = checkPlayListLength(chosenPlayList);
-        
-        searchForSong();
-        addSong(); // add first song on the returned list
+        //String len = checkPlayListLength(chosenPlayList);
+
+        searchForSong(songToSearch);
+        String actualString = addSong(); // add first song on the returned list
 
         // Verify/validate Song was added to chosen playlist via assertion
-        String len2 = checkPlayListLength(chosenPlayList); // Check length of playlist after add-song attempt
-        Assert.assertNotSame(len2,len);
+        // String len2 = checkPlayListLength(chosenPlayList); // Check length of playlist after add-song attempt
+        //Assert.assertNotSame(len2,len);
+        String expectedString = "Added 1 song into \"LeonPlayList.\"";
+        // Verify and validate that song was added to playlist via assertion of a message pop-up
+        Assert.assertEquals(actualString,expectedString);
     }
 
-    private String checkPlayListLength(String name) {
+    private String checkPlayListLength(String songName) {
         String url = "https://qa.koel.app/#!/home";
         driver.get(url);
         WebElement playList =
                 driver.findElement(By.xpath("//section[@id='playlists']/ul/li[3]/a[contains(text(),'LeonPlayList')]"));
+//        WebElement playList =
+//                driver.findElement(By.xpath("//section[@id='playlists']/ul/li[3]/a[contains(text(),songName)]"));
         playList.click();
         WebElement songCountText = driver.findElement(By.xpath("//section[@id='playlistWrapper']//span[contains(text(),'song')]"));
         return songCountText.getText();
     }
 
-    private void addSong() {
+    private String addSong() {
         WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//div[@class='item-container']//tr[1]"));
         firstSong.click();
         WebElement addToBtn = driver.findElement(By.xpath("//button[@class='btn-add-to']"));
@@ -42,14 +47,16 @@ public class Homework17 extends BaseTest {
         // Find and select the playlist we want to add the song to
         WebElement chosenPlayList = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//ul/li[contains(text(),chosenPlayList)]"));
         chosenPlayList.click(); // song added
+        WebElement songAddMessage = driver.findElement(By.xpath("//div[@class='alertify-logs top right']/div[@class='success show']"));
+        return songAddMessage.getText().toString();
     }
 
-    private void searchForSong() {
+    private void searchForSong(String songToSearch) {
         // Locate the Search Text Field Web Element
         driver.get("https://qa.koel.app/#!/search");
         WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
         searchField.clear();
-        searchField.sendKeys("dark");
+        searchField.sendKeys(songToSearch);
 
         // Locate "View All" button Web element
         // WebElement viewAllBtn = driver.findElement(By.cssSelector("button[data-testid='home-view-all-recently-played-btn']"));
