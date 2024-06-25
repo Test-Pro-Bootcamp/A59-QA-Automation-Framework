a7a24c16c90b0bfdb20b240abc49dc71e7f42386
testprolenp <leon.poyau@testpro.io>
2024-06-25 17:57:49 -0400

    Completed Homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -4,106 +4,74 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
-
+    @Test
     public void addSongToPlayList() throws InterruptedException {
-        // navigate to koel login page
+        String expectedSongAddedMessage = "Added 1 song into \"LeonPlayList.\"";
+
+        // Step 1 - navigate to koel login page
         navigateToPage();
 
-        // login
+        // Step 2 - login
         enterEmail("leon.poyau@testpro.io");
         enterPassword("jTRCkwNf");
         submit();
 
-        // search for a song
+        // Step 3 - search for a song
         searchSong("dark");
-        //click view-all button
-        // select first song from search results
-        // click Add-to button
-        // choose a playlist from the menu
+        Thread.sleep(1000);
+
+        //Step 4 - click view-all button
+        clickViewAllBtn();
+        Thread.sleep(1000);
+
+        // Step 5 - select first song from search results
+        selectFirstSong();
+        Thread.sleep(1000);
+
+        // Step 6 - click Add-to-button
+        clickAddToBtn();
+        Thread.sleep(1000);
+
+        // Step 7 - choose a playlist from the menu
+        choosePlaylist();
+        Thread.sleep(1000);
+
         // actual vs expected via assertion
+        Assert.assertEquals(getAddToPlayListMessage(),expectedSongAddedMessage);
 
 
     }
 
-    public void searchSong(String name) {
+    public String getAddToPlayListMessage() {
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        // return notification.getText().toString();
+        return notification.getText();
     }
-//    @Test
-//    public void addSongToPlaylist() throws InterruptedException {
-//        // Pre-requisite values
-//        String chosenPlayList = "LeonPlayList";
-//        String songToSearch = "dark";
-//
-//        navigateToHomePage();
-//        login();
-//
-//        // Check initial length of playlist
-//        //String len = checkPlayListLength(chosenPlayList);
-//
-//        searchForSong(songToSearch);
-//        String actualString = addSong(); // add first song on the returned list
-//
-//        // Verify/validate Song was added to chosen playlist via assertion
-//        // String len2 = checkPlayListLength(chosenPlayList); // Check length of playlist after add-song attempt
-//        //Assert.assertNotSame(len2,len);
-//        String expectedString = "Added 1 song into \"LeonPlayList.\"";
-//        // Verify and validate that song was added to playlist via assertion of a message pop-up
-//        Assert.assertEquals(actualString,expectedString);
-//    }
-//
-//    private String checkPlayListLength(String songName) {
-//        String url = "https://qa.koel.app/#!/home";
-//        driver.get(url);
-//        WebElement playList =
-//                driver.findElement(By.xpath("//section[@id='playlists']/ul/li[3]/a[contains(text(),'LeonPlayList')]"));
-////        WebElement playList =
-////                driver.findElement(By.xpath("//section[@id='playlists']/ul/li[3]/a[contains(text(),songName)]"));
-//        playList.click();
-//        WebElement songCountText = driver.findElement(By.xpath("//section[@id='playlistWrapper']//span[contains(text(),'song')]"));
-//        return songCountText.getText();
-//    }
-//
-//    private String addSong() {
-//        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//div[@class='item-container']//tr[1]"));
-//        firstSong.click();
-//        WebElement addToBtn = driver.findElement(By.xpath("//button[@class='btn-add-to']"));
-//        addToBtn.click();
-//        // Find and select the playlist we want to add the song to
-//        WebElement chosenPlayList = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//ul/li[contains(text(),chosenPlayList)]"));
-//        chosenPlayList.click(); // song added
-//        WebElement songAddMessage = driver.findElement(By.xpath("//div[@class='alertify-logs top right']/div[@class='success show']"));
-//        return songAddMessage.getText().toString();
-//    }
-//
-//    private void searchForSong(String songToSearch) {
-//        // Locate the Search Text Field Web Element
-//        driver.get("https://qa.koel.app/#!/search");
-//        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
-//        searchField.clear();
-//        searchField.sendKeys(songToSearch);
-//
-//        // Locate "View All" button Web element
-//        // WebElement viewAllBtn = driver.findElement(By.cssSelector("button[data-testid='home-view-all-recently-played-btn']"));
-//        WebElement viewAllBtn = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
-//        viewAllBtn.click();
-//    }
-//
-//    private void login() throws InterruptedException {
-//        // Locate Web elements Email field and Password field
-//        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-//        emailField.clear();
-//        emailField.sendKeys("leon.poyau@testpro.io");
-//
-//        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-//        passwordField.clear();
-//        passwordField.sendKeys("jTRCkwNf");
-//        WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
-//        loginBtn.click();
-//    }
-//
-//
-//    public void navigateToHomePage() throws InterruptedException {
-//        String url = "https://qa.koel.app/";
-//        driver.get(url);
-//        Thread.sleep(1000);
-//    }
+
+    public void choosePlaylist() {
+        WebElement playlist = driver.findElement
+                (By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'LeonPlayList')]"));
+                playlist.click();
+    }
+
+    public void clickAddToBtn() {
+        WebElement addToBtn = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@class='btn-add-to']"));
+        addToBtn.click();
+    }
+
+    public void selectFirstSong() {
+        WebElement firstSongReturned = driver.findElement
+                (By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        firstSongReturned.click();
+    }
+
+    public void clickViewAllBtn() {
+        WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
+        viewAllBtn.click();
+    }
+
+    public void searchSong(String songName) {
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type='search']"));
+        searchField.sendKeys(songName);
+    }
 }
