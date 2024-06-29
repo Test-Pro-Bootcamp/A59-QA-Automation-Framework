c83361f57b062dcc78971c0b62c1b993e5049780
Tootienahs <christina.taylor@testpro.io>
2024-06-29 01:15:53 -0400

    updated HW17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -4,8 +4,61 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
-
     @Test
+    public void addSongToPlaylist() throws InterruptedException {
+
+        String expectedSongAddedMessage = "Added 1 song into \"Test Pro Playlist.\"";
+
+        navigateToPage();
+        enterEmail("christina.taylor@testpro.io");
+        enterPassword("jKV0uSX6z1dv");
+        submit();
+        Thread.sleep(2000);
+        searchSong("Pluto");
+        clickViewAllBtn();
+        selectFirstSongResult();
+        clickAddToBtn();
+        choosePlaylist("lazy Days");
+        Assert.assertEquals(getAddToPlaylistSuccessMsg(), expectedSongAddedMessage);
+    }
+
+    public void searchSong(String name) throws InterruptedException {
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type='search']"));
+        searchField.sendKeys(name);
+        Thread.sleep(2000);
+    }
+    public void clickViewAllBtn() throws InterruptedException {
+        WebElement viewAll = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
+        viewAll.click();
+        Thread.sleep(2000);
+    }
+    public void selectFirstSongResult() throws InterruptedException {
+        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        firstSong.click();
+        Thread.sleep(2000);
+    }
+    public void clickAddToBtn() throws InterruptedException {
+        WebElement addToButton = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
+        addToButton.click();
+        Thread.sleep(2000);
+    }
+    public void choosePlaylist (String lazyDays) throws InterruptedException {
+        //We created a playlist named "Test Pro Playlist"
+        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Test Pro Playlist')]"));
+        playlist.click();
+        Thread.sleep(2000);
+    }
+    public String getAddToPlaylistSuccessMsg() {
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+    }
+}
+
+/*
+
+public class Homework17 extends BaseTest {
+
+
 
     public void addSongToPlaylist() throws InterruptedException {
 
@@ -80,3 +133,5 @@ public class Homework17 extends BaseTest {
         searchField.sendKeys(name);
     }
 }
+
+*/
