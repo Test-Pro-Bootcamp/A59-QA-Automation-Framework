12c731e6517c39c339be99f499445758924b1ec7
testprolenp <leon.poyau@testpro.io>
2024-07-10 23:05:56 -0400

    Completed Homework19

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -8,8 +8,11 @@ public class Homework19 extends BaseTest{
     @Test
     @Parameters({"BaseURL"})
     public void deletePlayList(String baseURL) throws InterruptedException {
-        // navigate to login page for Koel app
+        String expectedPlaylistDeletedMessage = "Deleted playlist \"LeonPlayList2.\"";
+
+        // Pre-condition - navigate to login page for Koel app
         navigateToPage(baseURL);
+
         // Step 1 - Login
         enterEmail("leon.poyau@testpro.io");;
         enterPassword("jTRCkwNf");
@@ -21,22 +24,28 @@ public class Homework19 extends BaseTest{
 
         // Step 3 - Click on Delete playlist button
         clickDelPlaylistBtn();
+        Thread.sleep(2000);
+
+        /*
+        //Step 4 - Click on OK button on dialog box to confirm intent to delete
+        clickOkToDelete();
         Thread.sleep(1000);
+        */
 
-        // Step 4 - Click on OK button on dialog box to confirm intent to delete
-//        clickOkToDelete();
-//        Thread.sleep(1000);
-
-        // Validate and verify playlist deleted by assertion.
-        String msg = "\"Deleted playlist {LeonPlayList2.}\"";
-        WebElement mesgDelModal = driver.findElement(
-                By.xpath("//div[contains(text(),\"Deleted playlist \\\"LeonPlayList2.\\\"\")]"));
-        Assert.assertTrue(mesgDelModal.isDisplayed());
+        // Validate and verify playlist deleted via actual vs expected assertion.
+        Assert.assertEquals(getDelPlayListMessage(),expectedPlaylistDeletedMessage);
     }
 
+    private String getDelPlayListMessage() {
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+
+    }
+
+    // This method is to be used if Playlist is not empty and requires Ok confirm on delete dialog box
     public void clickOkToDelete() {
         WebElement okBtn = driver.findElement(
-                By.xpath("clickDelPlaylist"));
+                By.xpath("//div[@class='dialog']//button[@class='ok']"));
             okBtn.click();
     }
 
@@ -48,7 +57,7 @@ public class Homework19 extends BaseTest{
 
     public void clickOnPlaylist() {
         WebElement playList = driver.findElement(
-                By.xpath("//section[@id='playlists']//a[@href='#!/playlist/97100']"));
+                By.xpath("//section[@id='playlists']//ul/li[4]/a"));
         playList.click();
     }
 }
