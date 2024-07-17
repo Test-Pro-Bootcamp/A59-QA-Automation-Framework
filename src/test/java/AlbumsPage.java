2b1abcca445ec9e82d76495e242a11ff7470ce27
Rusana <shikhabidovarusana@gmail.com>
2024-07-18 02:27:54 +1000

    ON Work
    On Work

--- src/test/java/AlbumsPage.java
+++ src/test/java/AlbumsPage.java
@@ -6,17 +6,25 @@ import org.testng.annotations.Test;
 
 public class AlbumsPage extends BaseTest{
 
-    @Test
-    public void albumsPage() throws InterruptedException {
-       enterEmail("shikhabidovarusana@gmail.com");
-        enterPassword("te$tstudent");
-        clickSubmit();
-        Assert.assertTrue( btnAlbumsPage());
 
+
+    @Test
+    public void testLogin() throws InterruptedException {
+
+        enterEmail("shikhabidovarusana@gmail.com");
+        enterPassword("te$t$tudent");
+        clickSubmit();
+        Assert.assertTrue(clickButtonAllSong());
 
     }
-    public boolean btnAlbumsPage(){
-        WebElement clickBtnAlbumsPAge = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class='albums']")));
-        return clickBtnAlbumsPAge.isDisplayed();
+    private boolean clickButtonAllSong () throws InterruptedException {
+        WebElement buttonAllSong = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class='songs']")));
+        Thread.sleep(500);
+        return buttonAllSong.isDisplayed();
+
     }
 }
+
+
+
+
