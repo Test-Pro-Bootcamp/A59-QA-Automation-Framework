fcf915be788f625caca0505ebd47eb2daf7d0a30
testprolenp <leon.poyau@testpro.io>
2024-08-16 21:46:28 -0400

    Updated HomeTest with songCount() and check count against Song info detail using getPlaylistInfo()

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.util.List;
 
 public class HomeTest extends BaseTest {
 
@@ -40,18 +41,42 @@ public class HomeTest extends BaseTest {
         // Step 2 - Find specific playlist element and click on it to select it. choose a playlist by name
         choosePlaylistByName("TestPro Playlist");
 
-        // Display all songs
+        // Step 3 - Display all songs and number of songs in the list.
+        // Here Use of driver.findElements and store in a List of WebElements.
+        displayAllSongs();
 
         // Verify number of songs in the playlist equals to what is displayed in the info section
-        // Step 3 - Count the number of songs in the play list
-        
+        // Count the number of songs in the play list against Songs Number in the Info section
+        Assert.assertTrue(getPlaylistInfo().contains(String.valueOf(songsCount())));
+
+    }
+
+    // Helper Methods
+    public String getPlaylistInfo() {
+        return driver.findElement(By.cssSelector("span.meta.text-secondary span.meta")).getText();
+    }
+
+    public int songsCount() {
+        return driver.findElements(By.cssSelector("section#playlistWrapper td.title")).size();
+    }
+
+    public void displayAllSongs() {
+        List<WebElement> songList = driver.findElements
+                (By.cssSelector("section#playlistWrapper td.title"));
+        // Display Song count
+        System.out.println("Number of Songs found: " + songsCount());
+
+        // Display each song title in the songList List of WebElement in the console
+        for(WebElement e: songList) {
+            System.out.println(e.getText());
+        }
     }
 
     public void choosePlaylistByName(String playlistName) {
 //        WebElement playlist = wait.until(
 //                ExpectedConditions.visibilityOfElementLocated(By.xpath("//section[@id='playlists']//li[4]")));
                 wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.xpath("//section[@id='playlists']//li[contains(text(),'"+playlistName+"')]"))).click();
+                (By.xpath("//section[@id='playlists']//a[contains(text(),'"+playlistName+"')]"))).click();
     }
 
     public boolean isSongPlaying() {
