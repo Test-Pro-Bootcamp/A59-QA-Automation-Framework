d95775f099e3cec2bab18d24134bbb740c2ba8ad
Apoorva Singh <singhapoorva03@gmail.com>
2024-08-28 22:07:02 -0700

    Homework-17 completed

--- src/test/java/HomeWork17.java
+++ src/test/java/HomeWork17.java
@@ -1,12 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-
-public class Homework17 extends BaseTest {
+public class HomeWork17 extends BaseTest {
 
     @Test
     public void addSongToPlaylist() throws InterruptedException {
