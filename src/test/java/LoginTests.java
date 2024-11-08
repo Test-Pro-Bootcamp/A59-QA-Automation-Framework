4e6a7a9fe89649d782b747c8257e5719f46b792e
Apoorva Singh <singhapoorva03@gmail.com>
2024-11-08 15:45:38 -0800

    Homework-19

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,47 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoginTests extends BaseTest {
+
     @Test
-    public void loginEmptyEmailPassword() {
+    public void loginValidEmailPassword() throws InterruptedException {
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        navigateToPage();
+        enterEmail("apurva.singh@testpro.io");
+        enterPassword("te$tpro$tudent1");
+        submit();
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        // Expected Result
+        Assert.assertTrue(avatarIcon.isDisplayed());
+    }
 
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    @Test
+    public void loginInvalidEmailValidPassword() throws InterruptedException {
+
+        navigateToPage();
+
+        // Steps
+        enterEmail("invalid@testpro.io");
+        enterPassword("te$tpro$tudent1");
+        submit();
+
+        Thread.sleep(2000); // Sleep or pause for 2 seconds (adjust as needed)
+        // Expected Result
+        Assert.assertEquals(driver.getCurrentUrl(), url); // https://qa.koel.app/
+    }
+
+    @Test
+    public void loginValidEmailEmptyPassword() throws InterruptedException {
+
+        navigateToPage();
+        enterEmail("invalid@testpro.io");
+        submit();
+
+        Thread.sleep(2000); // Sleep or pause for 2 seconds (adjust as needed)
+        // Expected Result
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
     }
 }
