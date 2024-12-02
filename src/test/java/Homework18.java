c21afe87a1d0d11538e4d269979ce30e49a607df
CarlaBre23 <Breanacfulton@gmail.com>
2024-12-01 22:25:26 -0500

    Create Homework18.java

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+    @Test
+    public void playSong() {
+        provideEmail("demo@testpro.io");
+        providePassword("")
+    }
+}
