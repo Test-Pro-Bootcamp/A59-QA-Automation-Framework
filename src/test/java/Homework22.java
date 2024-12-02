a01829e3b57082c281e9bc1d02126144bd426585
CarlaBre23 <Breanacfulton@gmail.com>
2024-12-02 00:10:34 -0500

    Update Homework22.java

--- src/test/java/Homework22.java
+++ src/test/java/Homework22.java
@@ -1,15 +1,5 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
-
-public class Homework22 {
-}import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
